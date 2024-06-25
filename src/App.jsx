fad2e0a90d6046afe7939f2b0b709faac305dfd7
Srinjoy Rakshit <srinjoyrakshit2003@gmail.com>
2024-06-25 13:05:58 +0530

    files uploaded

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-black h-screen w-full text-white'>
+      <h1 className='pt-5 text-3xl text-orange-600 text-bold text-center'>Ichigo Kurosaki</h1> 
+      <p className='text-xl p-10 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ut sed dolorem, corporis dolor delectus Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati temporibus dolore itaque sint nemo voluptatem est praesentium nihil pariatur rem?</p>
+    </div>
+  
+  )
+}
+
+export default App
